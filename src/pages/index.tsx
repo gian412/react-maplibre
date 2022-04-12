7c5bd5bddc831285d66eaede816ffb54f56aa4b2
gian412 <r.gian@protonmail.com>
2022-04-12 09:08:51 +0200

    feat(pages): intergrate map component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import type { NextPage } from 'next';
-import {
-    Box,
-    Flex,
-    FormControl,
-    FormLabel,
-    Grid,
-    GridItem,
-    Heading,
-    IconButton,
-    Input,
-    Text,
-    Textarea,
-} from '@chakra-ui/react';
-import { ArrowRightIcon, EmailIcon, InfoIcon, PhoneIcon } from '@chakra-ui/icons';
-import { Contacts, Disclaimer, Footer, Intro } from '../components';
+import { Box } from '@chakra-ui/react';
+import { Contacts, Disclaimer, Footer, Intro, Map } from '../components';
 
 const Home: NextPage = () => {
     return (
         <Box position='relative'>
             <Intro />
             <Contacts />
+            <Map />
             <Disclaimer />
             <Footer />
         </Box>
